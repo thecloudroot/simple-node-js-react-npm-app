aafb65d28fd722087de7a849832cdc1fe5e177e8
thecloudroot <62428182+thecloudroot@users.noreply.github.com>
2020-08-07 12:08:56 +0300

    Add files via upload

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,7 +5,7 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 #set -x
-npm install
+npm clean-install
 npm run build
 #set +x
 echo 'The following "npm" command runs your Node.js/React application in'
