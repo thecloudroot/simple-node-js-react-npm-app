f5dd1e42ab04ed7e40ef1a1bac4b89ebf84ee144
thecloudroot <62428182+thecloudroot@users.noreply.github.com>
2020-08-07 11:17:56 +0300

    Add files via upload
    asd

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-
 echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
@@ -9,16 +8,12 @@ set -x
 npm run build
 npm install -g serve
 set +x
-
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
-
 set -x
 serve -s build -l 3000 &
 sleep 1
 echo $! > .pidfile
 set +x
-
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-
