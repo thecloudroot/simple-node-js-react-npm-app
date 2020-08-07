afe9c5d57345ac4ee7eda6912ec68be56c42bb67
thecloudroot <62428182+thecloudroot@users.noreply.github.com>
2020-08-07 11:35:15 +0300

    Add files via upload

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,13 +4,13 @@ echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
-set -x
+#set -x
 npm run build
 npm install -g serve
-set +x
+#set +x
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
-set -x
+#set -x
 serve -s build -l 3000 &
 sleep 1
 echo $! > .pidfile
