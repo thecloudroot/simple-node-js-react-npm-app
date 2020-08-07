a87a675ab891912d0ef6f2f2d00965c0be69a190
thecloudroot <62428182+thecloudroot@users.noreply.github.com>
2020-08-07 11:41:52 +0300

    Add files via upload

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -14,6 +14,6 @@ echo 'development mode and makes the application available for web browsing.'
 serve -s build -l 3000 &
 sleep 1
 echo $! > .pidfile
-set +x
+#set +x
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
